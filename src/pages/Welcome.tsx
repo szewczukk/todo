26985cdb5fca8ed1fd0a751e08316592906bc235
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-05 14:10:20 +0200

    Typography variant of Welcome Page changed

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,7 +2,7 @@ import { Typography } from '@material-ui/core';
 import React from 'react';
 
 const WelcomeScreen = () => (
-	<Typography variant="h4" component="h1" data-testid="heading">
+	<Typography variant="h5" component="h1" data-testid="heading">
 		Choose a page from the navigation on left
 	</Typography>
 );
