d1017bd26081d3326c82666ae065157810d3d2b4
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-05 13:54:52 +0200

    Testing a drawer navigation existence

--- cypress/integration/e2e/WelcomeScreen.spec.ts
+++ cypress/integration/e2e/WelcomeScreen.spec.ts
@@ -2,6 +2,10 @@ describe('Welcome screen testing', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:3000/');
 	});
+
+	it('The navigation contains a link to welcome screen', () => {
+		cy.get('[data-testid="navigation"]').should('contain', 'Welcome');
+	});
 	it('The site contains a heading', () => {
 		cy.get('[data-testid="heading"]').should(
 			'contain.text',
