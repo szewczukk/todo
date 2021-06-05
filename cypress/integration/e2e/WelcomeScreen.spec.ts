336d522dd01d946edc12a146a8961954f52cbd3f
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-05 13:17:51 +0200

    The test patched

--- cypress/integration/e2e/WelcomeScreen.spec.ts
+++ cypress/integration/e2e/WelcomeScreen.spec.ts
@@ -2,10 +2,10 @@ describe('Welcome screen testing', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:3000/');
 	});
-	it('The site a heading', () => {
+	it('The site contains a heading', () => {
 		cy.get('[data-testid="heading"]').should(
 			'contain.text',
-			'Choose a page from navigation on left',
+			'Choose a page from the navigation on left',
 		);
 	});
 });
