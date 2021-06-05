1f02ffaffd7e81302c6127a20aa8fdd552a0c666
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-05 13:14:20 +0200

    Simple routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import WelcomeScreen from './pages/Welcome';
 
 function App() {
-	return <h1>App</h1>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact>
+					<WelcomeScreen />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
