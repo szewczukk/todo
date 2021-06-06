5829de81ad58f3c0bf483513e14f2abb2fa28e3f
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-06 12:26:59 +0200

    WelcomeScreen test renamed to WelcomePage

--- cypress/integration/e2e/WelcomePage.spec.ts
+++ cypress/integration/e2e/WelcomePage.spec.ts
@@ -1,4 +1,4 @@
-describe('Welcome screen testing', () => {
+describe('Welcome page testing', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:3000/');
 	});
@@ -6,6 +6,7 @@ describe('Welcome screen testing', () => {
 	it('The navigation contains a link to welcome screen', () => {
 		cy.get('[data-testid="navigation"]').should('contain', 'Welcome');
 	});
+
 	it('The site contains a heading', () => {
 		cy.get('[data-testid="heading"]').should(
 			'contain.text',
