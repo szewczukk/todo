97a18aa0cc96fe37df7b31822a74484554e833bb
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-06 22:46:40 +0200

    Test for reaching the intended task added

---
+++ cypress/integration/e2e/PreviewTaskPage.spec.ts
@@ -0,0 +1,19 @@
+describe('Task list page testing', () => {
+	before(() => {
+		cy.visit('http://localhost:3000/');
+	});
+
+	it('The page should render a table with the info about the task', () => {
+		cy.get('[data-testid="taskslist"]').click();
+		cy.get('[data-testid="description"]').type('Hello, world!');
+		cy.get('[data-testid="createTaskForm"]').submit();
+
+		cy.get('[data-testid="main"]')
+			.contains('Hello, world!')
+			.parent()
+			.contains('Edit')
+			.click();
+
+		cy.get('[data-testid="main"]').should('contain', 'Hello, world!');
+	});
+});
