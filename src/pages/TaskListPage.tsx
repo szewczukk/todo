56c1cd4185d4f8d9a232e8013baf4fcaed849f06
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-06 14:06:49 +0200

    TaskListPage is now gridded

--- src/pages/TaskListPage.tsx
+++ src/pages/TaskListPage.tsx
@@ -1,12 +1,19 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
 import CreateTaskForm from '../components/CreateTaskForm';
 import TaskTable from '../components/TaskTable';
 
-const TaskListPage = () => (
-	<>
-		<TaskTable />
-		<CreateTaskForm />
-	</>
-);
+const TaskListPage = () => {
+	return (
+		<Grid container spacing={3}>
+			<Grid item xs={12} md={6}>
+				<TaskTable />
+			</Grid>
+			<Grid item xs={12} md={6}>
+				<CreateTaskForm />
+			</Grid>
+		</Grid>
+	);
+};
 
 export default TaskListPage;
