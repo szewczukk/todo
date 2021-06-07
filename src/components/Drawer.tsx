fb7598695cca94ab14f2997ad98e2d973b898fd0
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-07 13:35:31 +0200

    Drawer splitted to two components

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,34 +1,21 @@
 import React from 'react';
 import {
 	createStyles,
-	Divider,
 	Drawer as MaterialDrawer,
-	List,
-	ListItem,
-	ListItemIcon,
-	ListItemText,
 	makeStyles,
 	Toolbar,
 } from '@material-ui/core';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import { Link, useLocation } from 'react-router-dom';
-import HomeIcon from '@material-ui/icons/Home';
-import { FormatListBulleted } from '@material-ui/icons';
-import { useAppSelector } from '../store';
+import DrawerContent from './DrawerContent';
 
 const useStyles = makeStyles(() =>
 	createStyles({
 		drawer: { width: 240, flexShrink: 0 },
 		drawerPaper: { width: 240 },
-		drawerContainer: { overflow: 'auto' },
-		link: { color: 'inherit', textDecoration: 'none' },
 	}),
 );
 
 const Drawer = () => {
-	const { pathname } = useLocation();
 	const classes = useStyles();
-	const { tasks } = useAppSelector((store) => store);
 
 	return (
 		<MaterialDrawer
@@ -39,49 +26,7 @@ const Drawer = () => {
 			data-testid="navigation"
 		>
 			<Toolbar />
-			<div className={classes.drawerContainer}>
-				<List>
-					<Link to="/" className={classes.link}>
-						<ListItem button>
-							<ListItemIcon>
-								<HomeIcon color={pathname === '/' ? 'primary' : 'inherit'} />
-							</ListItemIcon>
-							<ListItemText>Welcome</ListItemText>
-						</ListItem>
-					</Link>
-					<Divider />
-					<Link to="/list" className={classes.link} data-testid="taskslist">
-						<ListItem button>
-							<ListItemIcon>
-								<FormatListBulleted
-									color={pathname === '/list' ? 'primary' : 'inherit'}
-								/>
-							</ListItemIcon>
-							<ListItemText>List of tasks</ListItemText>
-						</ListItem>
-					</Link>
-					{tasks.map((task, idx) => (
-						<Link
-							to={`/list/${task.id}`}
-							className={classes.link}
-							key={task.id}
-						>
-							<ListItem button>
-								<ListItemIcon>
-									<AssignmentIcon
-										color={
-											pathname.split('/').includes(task.id)
-												? 'primary'
-												: 'inherit'
-										}
-									/>
-								</ListItemIcon>
-								<ListItemText>Task no {idx + 1}</ListItemText>
-							</ListItem>
-						</Link>
-					))}
-				</List>
-			</div>
+			<DrawerContent />
 		</MaterialDrawer>
 	);
 };
---
+++ src/components/DrawerContent.tsx
@@ -0,0 +1,70 @@
+import {
+	createStyles,
+	Divider,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+	makeStyles,
+} from '@material-ui/core';
+import { FormatListBulleted, Home, Assignment } from '@material-ui/icons';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useAppSelector } from '../store';
+
+const useStyles = makeStyles(() =>
+	createStyles({
+		drawerContainer: { overflow: 'auto' },
+		link: { color: 'inherit', textDecoration: 'none' },
+	}),
+);
+
+const DrawerContent = () => {
+	const classes = useStyles();
+	const { pathname } = useLocation();
+	const { tasks } = useAppSelector((store) => store);
+
+	return (
+		<div className={classes.drawerContainer}>
+			<List>
+				<Link to="/" className={classes.link}>
+					<ListItem button>
+						<ListItemIcon>
+							<Home color={pathname === '/' ? 'primary' : 'inherit'} />
+						</ListItemIcon>
+						<ListItemText>Welcome</ListItemText>
+					</ListItem>
+				</Link>
+				<Divider />
+				<Link to="/list" className={classes.link} data-testid="taskslist">
+					<ListItem button>
+						<ListItemIcon>
+							<FormatListBulleted
+								color={pathname === '/list' ? 'primary' : 'inherit'}
+							/>
+						</ListItemIcon>
+						<ListItemText>List of tasks</ListItemText>
+					</ListItem>
+				</Link>
+				{tasks.map((task, idx) => (
+					<Link to={`/list/${task.id}`} className={classes.link} key={task.id}>
+						<ListItem button>
+							<ListItemIcon>
+								<Assignment
+									color={
+										pathname.split('/').includes(task.id)
+											? 'primary'
+											: 'inherit'
+									}
+								/>
+							</ListItemIcon>
+							<ListItemText>Task no {idx + 1}</ListItemText>
+						</ListItem>
+					</Link>
+				))}
+			</List>
+		</div>
+	);
+};
+
+export default DrawerContent;
