b122c2de2687fc548709793e957993339256f389
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2021-06-07 13:47:18 +0200

    Two drawers created: one for mobile and one for desktop

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import {
 	createStyles,
 	Drawer as MaterialDrawer,
+	Hidden,
 	makeStyles,
 	Toolbar,
 } from '@material-ui/core';
 import DrawerContent from './DrawerContent';
+import { useAppDispatch, useAppSelector } from '../store';
+import { actions as drawerActions } from '../store/drawerSlice';
 
 const useStyles = makeStyles(() =>
 	createStyles({
@@ -15,19 +18,51 @@ const useStyles = makeStyles(() =>
 );
 
 const Drawer = () => {
+	const dispatch = useAppDispatch();
+	const { open } = useAppSelector((state) => state.drawer);
 	const classes = useStyles();
 
+	const container =
+		window !== undefined ? () => window.document.body : undefined;
+
+	const toggleDrawer = () => {
+		dispatch(drawerActions.toggleOpenness());
+	};
+
 	return (
-		<MaterialDrawer
-			variant="permanent"
-			anchor="left"
-			className={classes.drawer}
-			classes={{ paper: classes.drawerPaper }}
-			data-testid="navigation"
-		>
-			<Toolbar />
-			<DrawerContent />
-		</MaterialDrawer>
+		<>
+			<Hidden xsDown implementation="css">
+				<MaterialDrawer
+					open
+					variant="permanent"
+					anchor="left"
+					className={classes.drawer}
+					classes={{ paper: classes.drawerPaper }}
+					data-testid="navigation"
+				>
+					<Toolbar />
+					<DrawerContent />
+				</MaterialDrawer>
+			</Hidden>
+			<Hidden smUp implementation="css">
+				<MaterialDrawer
+					container={container}
+					variant="temporary"
+					anchor={'left'}
+					open={open}
+					onClose={toggleDrawer}
+					classes={{
+						paper: classes.drawerPaper,
+					}}
+					ModalProps={{
+						keepMounted: true,
+					}}
+				>
+					<Toolbar />
+					<DrawerContent />
+				</MaterialDrawer>
+			</Hidden>
+		</>
 	);
 };
 
